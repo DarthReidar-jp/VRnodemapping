69bc50f1bd92c4fee0464d4786e5138a956e7d13
RPC1Mac <>
2022-12-31 23:05:15 +0900

    NodeConを作った

---
+++ Assets/Script/Main/Node/NodeController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeController : MonoBehaviour
+{
+     //カーソルで選んだNodeを移動させる。
+     void OnMouseDrag()
+    {
+        Vector3 objPos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, objPos.z);
+        transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+}
