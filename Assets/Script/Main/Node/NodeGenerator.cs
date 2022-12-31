a39074283e4395125c7e94d809f0785db5d30ddf
RPC1Mac <>
2022-12-31 20:13:43 +0900

    プレイヤーコントローラースクリプトの作成、nodeジェネレーターの作成、プレハブとか作成しました

---
+++ Assets/Script/Main/Node/NodeGenerator.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeGenerator : MonoBehaviour
+{
+    //Nodeプレハブの配列
+    public GameObject[] Nodes = new GameObject[3];
+    //現在のマウス座標
+    private Vector3 _mousePos;
+    // Nodeの座標
+	private Vector3 _nodePos;
+    //ダブルクリックの確認
+    private int _touchCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _touchCount++;
+            Invoke("DoubleClicks",0.3f);
+        }
+    }
+
+    void Generator ()
+    {
+        _mousePos = Input.mousePosition;
+		// Z軸を代入
+		_mousePos.z = 5f;
+		// マウス位置座標をスクリーン座標からワールド座標に変換する
+		_nodePos = Camera.main.ScreenToWorldPoint(_mousePos);
+		// ワールド座標に変換されたマウス座標を代入
+		Instantiate(Nodes[0],_nodePos,Quaternion.identity);
+    }
+
+      void DoubleClicks()
+   {
+       //ダブルタッチされているか
+       if(_touchCount != 2) { _touchCount = 0; return; }     
+       else{ _touchCount = 0; }
+       
+       //Nodeの生成
+       Generator();
+   }
+}
